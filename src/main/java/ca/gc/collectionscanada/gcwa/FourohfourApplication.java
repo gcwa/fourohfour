7933bea419aee1c1f1999b2be57d0fd0d5720252
Eric Leclerc <eric.leclerc2@canada.ca>
2016-10-28 15:26:27 -0400

    Initial commit

---
+++ src/main/java/ca/gc/collectionscanada/gcwa/FourohfourApplication.java
@@ -0,0 +1,12 @@
+package ca.gc.collectionscanada.gcwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourohfourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourohfourApplication.class, args);
+	}
+}
